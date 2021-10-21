e9df907f9556cebe99e90ff892b3efd766ce9137
brettsodie <blsodie@gmail.com>
2021-10-21 19:27:54 -0400

    fix: use async download

--- src/services/event/deploy/deploy.class.ts
+++ src/services/event/deploy/deploy.class.ts
@@ -21,7 +21,7 @@ export class Deploy {
     } catch(e) {}
     
     // Download source code
-    await download(`${githubUser}/${repo}#${branch}`, `'home/pi/apps/${name}`);
+    await downloadAsync(`${githubUser}/${repo}#${branch}`, `'home/pi/apps/${name}`);
 
     // Install node modules
     await executeCommand(`cd /home/pi/apps/${name} && npm i`);
